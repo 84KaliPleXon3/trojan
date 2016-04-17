f3cef94ceee331b03656a2407de0f0ffae1ed704
Hackerl <liupan052997@gmail.com>
2016-04-17 11:00:14 +0800

    ssh

--- tool/bh_sshcmd.py
+++ tool/bh_sshcmd.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 #连接远程ssh服务器，执行命令
+#上传下载文件
+
 import paramiko
 import sys
+import getopt
+import os
 
-def ssh_command(ip,user,passwd):
-	#初始化ssh客户端
-	client=paramiko.SSHClient()
-	#加载密钥
-	client.load_host_keys('/root/.ssh/known_hosts')
-	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	try:
-		#如果提供帐号密码
-		if user and passwd:
-			client.connect(ip,username=user,password=passwd)
-		else:
-			#密钥连接
-			client.connect(ip)
-	except:
-		print('connect failed!')
+command = False
+download = False
+upload = False
+user=''
+passwd=''
+target=''
+
+def ssh_command(client):
 	#主循环执行命令
 	while True:
 		try:
@@ -32,13 +29,111 @@ def ssh_command(ip,user,passwd):
 		except KeyboardInterrupt:
 			client.close()
 			sys.exit(0)
-def main():
-	try:
-		user=sys.argv[2]
-		passwd=sys.argv[3]
-	except Exception:
-		user=None
-		passwd=None
-	ssh_command(sys.argv[1],user,passwd)
+def ssh_upload(client):
+	transport=client.get_transport()
+	sftp = paramiko.SFTPClient.from_transport(transport)
+	#主循环执行命令
+	while True:
+		try:
+			localpath= raw_input("localpath:")
+			remotepath=raw_input("remotepath:") or ('/root/%s'%os.path.split(localpath)[1])
+			sftp.put(localpath,remotepath)
+			print '------------------------------------------------------------------------------'
+			print 'UPLOAD SUCCESS:'
+			print '[finished]local:%s >>>>> remote %s'%(localpath,remotepath)
+			print '------------------------------------------------------------------------------'
+		except KeyboardInterrupt:
+			client.close()
+			sys.exit(0)
+def ssh_download(client):
+	transport=client.get_transport()
+	sftp = paramiko.SFTPClient.from_transport(transport)
+	#主循环执行命令
+	while True:
+		try:
+			remotepath=raw_input("remotepath:")
+			localpath= raw_input("localpath:") or ('/root/%s'%os.path.split(remotepath)[1])
+			sftp.get(remotepath,localpath)
+			print '------------------------------------------------------------------------------'
+			print 'DOWNLOAD SUCCESS:'
+			print '[finished]local:%s <<<<< remote %s'%(localpath,remotepath)
+			print '------------------------------------------------------------------------------'
+		except KeyboardInterrupt:
+			client.close()
+			sys.exit(0)
 
+def ssh_client(target,user,passwd):
+	#初始化ssh客户端
+	client=paramiko.SSHClient()
+	#加载密钥
+	client.load_host_keys('/root/.ssh/known_hosts')
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	try:
+		#如果提供帐号密码
+		if len(user)>2 and len(passwd)>6:
+			client.connect(ip,username=user,password=passwd)
+		else:
+			#密钥连接
+			client.connect(target)
+		return client
+	except:
+		print('connect failed!')
+
+
+def usage():
+	print "ssh client"
+	print
+	print "Usage: bh_sshcmd.py -t ip [option]"
+	print "-t --target               - remote ip"
+	print "-c --command               - command shell"
+	print "-u --upload    - upon receiving connection upload a file and write to [destination]"
+	print "--user=****    - if you don't want to use rsa"
+	print "--passwd=****    - if you don't want to use rsa"
+	print
+	print
+	print "Examples: "
+	print "bh_sshcmd.py -t 192.168.0.1 -c"
+	print "bh_sshcmd.py -t 192.168.0.1 -u --user=**** --passwd=****"
+	sys.exit(0)
+
+def main():
+	global command
+	global download
+	global upload
+	global user
+	global passwd
+	global target
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hdcut:",
+								["help", "download", "command", "upload","user=","passwd=","target="])
+	except getopt.GetoptError as err:
+		print str(err)
+		usage()
+    
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+		elif o in ("-d", "--download"):
+			download = True
+		elif o in ("-c", "--command"):
+			command = True
+		elif o in ("-u", "--upload"):
+			upload = True
+		elif o in ("-t", "--target"):
+			target = a
+		elif o == "--user":
+			user = a
+		elif o == "--passwd":
+			passwd = a
+		else:
+			assert False, "Unhandled Option"
+
+	client=ssh_client(target,user,passwd)
+	if command:
+		ssh_command(client)
+	elif upload:
+		ssh_upload(client)
+	elif download:
+		ssh_download(client)
 main()

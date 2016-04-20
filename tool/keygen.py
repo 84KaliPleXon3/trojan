3c31827587c45f7c740d623cf4ed8413c2ba57e2
Hackerl <liupan052997@gmail.com>
2016-04-20 11:17:34 +0800

    KEY

---
+++ tool/keygen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+#生成公私钥
+
+from Crypto.PublicKey import RSA
+new_key = RSA.generate(2048, e=65537)
+public_key = new_key.publickey().exportKey("PEM")
+private_key = new_key.exportKey("PEM")
+print public_key
+print private_key
